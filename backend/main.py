24d0a8101e38a4bcb022162028480f0a328a7691
Rimocide <omega5667@gmail.com>
2026-07-03 23:12:33 +0500

    Wrote middleware exception handler

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from middlewares.exception_handlers import catch_exception_middleware
+
+app=FastAPI(title="Github AI Assistant API", description="API for RAG on Github Repos")
+
+# CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=["*"],
+    allow_methods=["*"],
+   allow_headers=["*"] 
+)
+
+app.middleware("http")(catch_exception_middleware)
